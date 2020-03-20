402a4eac34bd46ed4e2eb83cb12d040357e94690
flin-7 <flin.7@icloud.com>
2020-03-19 23:42:24 -0700

    Modal Presentation

--- DesignCode/DesignCode/HomeView.swift
+++ DesignCode/DesignCode/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct HomeView: View {
     @Binding var showProfile: Bool
+    @State private var showUpdate = false
     
     var body: some View {
         VStack {
@@ -20,6 +21,20 @@ struct HomeView: View {
                 Spacer()
                 
                 AvatarView(showProfile: $showProfile)
+                
+                Button(action: { self.showUpdate.toggle() } ) {
+                    Image(systemName: "bell")
+                        .renderingMode(.original)
+                        .font(.system(size: 16, weight: .medium))
+                        .frame(width: 36, height: 36)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                }
+                .sheet(isPresented: $showUpdate) {
+                    ContentView()
+                }
             }
             .padding(.horizontal)
             .padding(.leading, 14)
@@ -30,9 +45,9 @@ struct HomeView: View {
                     ForEach(sectionData) { item in
                         GeometryReader { geometry in
                             SectionView(section: item)
-                            .rotation3DEffect(Angle(degrees:
-                                Double(geometry.frame(in: .global).minX - 30) / -20
-                            ), axis: (x: 0, y: 10, z: 0))
+                                .rotation3DEffect(Angle(degrees:
+                                    Double(geometry.frame(in: .global).minX - 30) / -20
+                                ), axis: (x: 0, y: 10, z: 0))
                         }
                         .frame(width: 275, height: 275)
                     }
