1e0c151e0c69bbe8965a84a71c11dcb7b5b545ec
flin-7 <flin.7@icloud.com>
2020-03-21 13:59:52 -0700

    TabBar and Test on Device

---
+++ DesignCode/DesignCode/TabBar.swift
@@ -0,0 +1,34 @@
+//
+//  TabBar.swift
+//  DesignCode
+//
+//  Created by Felix Lin on 3/21/20.
+//  Copyright © 2020 Felix Lin. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TabBar().previewDevice("iPhone 8")
+            TabBar().previewDevice("iPhone 11 Pro Max")
+        }
+    }
+}
